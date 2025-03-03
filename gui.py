3e3aa8b8ffa1112de99053006838cb8c0d6e9d49
tckwgd <tckwgd@gmail.com>
2025-03-03 23:38:21 +0000

    test

--- gui.py
+++ gui.py
@@ -35,7 +35,7 @@ common_ui()
 
 st.markdown(f"<h1 style='text-align: center; font-weight:bold; font-family:comic sans ms; padding-top: 0rem;'> \
             {app_title}</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>基本配置信息</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>基本配置</h2>", unsafe_allow_html=True)
 
 if 'ui_language' not in st.session_state:
     st.session_state['ui_language'] = 'zh-CN - 简体中文'
